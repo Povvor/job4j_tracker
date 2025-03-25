6e7fe8faec5ab3e3651c4b69a3d82db351f0d7a1
Povvor <Povvor@yandex.ru>
2025-03-25 18:39:50 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String massage;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String massage) {
+        this.active = active;
+        this.status = status;
+        this.massage = massage;
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка активна: " + active);
+        System.out.println("Код ошибки: " + status);
+        System.out.println("Сообщение пользователю: " + massage + "\n");
+    }
+
+    public static void main(String[] args) {
+        Error error0 = new Error();
+        Error error401 = new Error(true, 401, "Требуется авторизация");
+        Error error403 = new Error(true, 403, "Доступ запрещен");
+        Error error404 = new Error(true, 404, "Ресурс не найден");
+        error0.printInfo();
+        error404.printInfo();
+        error401.printInfo();
+        error403.printInfo();
+    }
+
+}
