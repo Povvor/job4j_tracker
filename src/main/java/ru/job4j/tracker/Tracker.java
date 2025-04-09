55c1d27e9ddd1c89d14518c112f64f3f0c8115db
Povvor <Povvor@yandex.ru>
2025-04-09 20:32:20 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -65,6 +65,16 @@ public class Tracker {
         }
         result = Arrays.copyOf(result, localSize);
         return result;
-            }
+    }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int length = size - index - 1;
+            System.arraycopy(items, index + 1, items, index, length);
+            items[size - 1] = null;
+            size--;
         }
+    }
+}
 
