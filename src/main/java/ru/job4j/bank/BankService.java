10c62a0cb0da175ad9f4e8abb8c6cefa840b8e46
Povvor <Povvor@yandex.ru>
2025-05-14 20:33:10 +0300

    3. Банковские переводы [#10038 #543082]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,41 +1,29 @@
 package ru.job4j.bank;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BankService {
-    private final Map<User, List<Account>> users = new HashMap<>();
+    private final Map<User, HashSet<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        List<Account> accounts = new ArrayList<>();
+        HashSet<Account> accounts = new HashSet<>();
         users.putIfAbsent(user, accounts);
     }
 
     public void deleteUser(String passport) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            users.remove(user);
-        } else {
-            System.out.println("Данного пользователя не существует!");
-        }
-
+        User user = new User(passport, null);
+        users.remove(user);
     }
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        if (user != null && !users.get(user).contains(account)) {
+        if (user != null) {
             users.get(user).add(account);
-        } else {
-            System.out.println("Данного пользователя не существует!");
         }
     }
 
     public User findByPassport(String passport) {
-        User keyUser = new User(passport, null);
-        User[] userList = users.keySet().toArray(new User[0]);
-        for (User user : userList) {
-            if (user.equals(keyUser)) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
                 return user;
             }
         }
@@ -44,16 +32,16 @@ public class BankService {
 
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
+        Account output = null;
         if (user == null) {
-            System.out.println("Данного пользователя не существует!");
-            return null;
+            return output;
         }
         for (Account account : users.get(user)) {
             if (account.getRequisite().equals(requisite)) {
-                return account;
+                output = account;
             }
         }
-        return null;
+        return output;
     }
 
     public boolean transferMoney(String sourcePassport, String sourceRequisite,
@@ -75,7 +63,7 @@ public class BankService {
         return result;
     }
 
-    public List<Account> getAccounts(User user) {
+    public HashSet<Account> getAccounts(User user) {
         return users.get(user);
     }
 }
